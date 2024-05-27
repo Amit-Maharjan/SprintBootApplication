27d423a7042049e2f9c5ea1dd2d7cdc19e2d602d
Amit Maharjan <maharjanamit8@gmail.com>
2024-05-27 17:36:34 -0400

    Multiple ways to pass data

--- src/main/java/com/maharjan/amit/web/controller/HomeController.java
+++ src/main/java/com/maharjan/amit/web/controller/HomeController.java
@@ -4,7 +4,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -26,5 +28,25 @@ public class HomeController {
         HttpSession session = request.getSession();
         session.setAttribute("name", name);
         return "homePage";
+    }
+
+    @RequestMapping("/extraHome1")
+    public String extraHome1(String name, HttpSession session){
+        session.setAttribute("name", name);
+        return "homePage";
+    }
+
+    @RequestMapping("/extraHome2")
+    public String extraHome2(@RequestParam("name") String username, HttpSession session){
+        session.setAttribute("name", username);
+        return "homePage";
+    }
+
+    @RequestMapping("/extraHome3")
+    public ModelAndView extraHome3(@RequestParam("name") String username){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name", username);
+        mv.setViewName("homePage");
+        return mv;
     }
 }
